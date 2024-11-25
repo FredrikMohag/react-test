a8010e8603da21596c5685bc9542dbf2bef9b2c2
Fredrik Mohag Håkansson <frehak54268@stud.noroff.no>
2024-11-25 18:46:32 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
-import CheckoutPage from "./pages/CheckoutPage"; // Lägg till import för CheckoutPage
+import CheckoutPage from "./pages/CheckoutPage";
 import CheckoutSuccessPage from "./pages/CheckoutSuccessPage";
 import ContactPage from "./pages/ContactPage";
 import Layout from "./components/Layout";
